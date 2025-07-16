10056aa501216fcadd0909c9df7b9e2b5bef72e5
mayankgarg2572 <14mayankgarg2001@gmail.com>
2025-07-16 20:34:28 +0530

    Removed Unnecessary things from repo

--- backend/index.js
+++ backend/index.js
@@ -4,18 +4,16 @@ const cors = require('cors');
 const path = require('path'); // Import path module
 const connectDB = require('./config/db');
 const locationRoutes = require('./routes/locationRoutes');
-const itineraryRoutes = require('./routes/itineraryRoutes');
 
-// dotenv.config();
+
 dotenv.config({ path: path.resolve(__dirname, '.env') });
 const app = express();
 
-// Middleware
 app.use(express.json());
 
-// CORS configuration clearly allowing only specified frontend URL
+
 const corsOptions = {
-  origin: 'http://localhost:3000', // clearly your frontend URL
+  origin: 'http://localhost:3000',
   methods: 'GET, POST, PUT, DELETE',
   credentials: true,
 };
@@ -25,7 +23,6 @@ app.use(cors(corsOptions));
 
 // Routes
 app.use('/api', locationRoutes);
-app.use('/api', itineraryRoutes);
 
 // Connect to Database and Start Server
 const PORT = process.env.PORT || 5000;
--- backend/routes/itineraryRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getItinerary } = require('../controllers/itineraryController');
-
-router.post('/get-itinerary', getItinerary);
-
-module.exports = router;
